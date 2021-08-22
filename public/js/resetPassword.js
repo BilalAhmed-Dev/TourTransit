5c7bcedb1416336538338ebbff408bb90781c383
BilalAhmed-Dev <bilalahmedturk@gmail.com>
2021-08-21 20:20:20 -0400

    Initial commit

---
+++ public/js/resetPassword.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const resetPassword = async (password, passwordConfirm, token) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: `http://127.0.0.1:3000/api/v1/users/resetPassword/${token}`,
+      data: {
+        password,
+        passwordConfirm,
+      },
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', 'An email has been sent!');
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
